8176e8758bc22c5a3b8ecb2806bcf1a6c7a092a0
G7AX <alex709669@mail.ru>
2022-03-13 08:33:13 +0900

    Добавил удаление команды

--- discordBot.py
+++ discordBot.py
@@ -4,28 +4,32 @@ from random import choice
 from os import listdir
 from config import settings
 
-
 bot = commands.Bot(command_prefix=settings['prefix'])# символ после которого бот будет реагировать на команду
 
 @bot.event
-
 async def on_ready():# проверка включился ли бот
     print('Bot connected')
 
 @bot.command()# включает prefix для бота
-
 async def repeat(ctx, arg):# !repeat повтор введённого сообщения
+    await ctx.message.delete()
     author = ctx.message.author
     await ctx.send(f" { author.mention } " + arg)
 
 @bot.command()
-
 async def img(ctx):
+    await ctx.message.delete()
     path = open('pathToPictures.txt', 'r').readline()
     files = listdir(path)
+    author = ctx.message.author
     randomizer = choice(files)
-    await ctx.send('Держи', file=discord.File(f'{path}{randomizer}', spoiler=False))# Если хочешь, чтобы не было на пикче "спойлер. Удали ", spoiler=True"
+    await ctx.channel.send(f'{author.mention}Держи', file=discord.File(f'{path}{randomizer}', spoiler=False))# Если хочешь, чтобы не было на пикче "спойлер. Удали ", spoiler=True"
 
-#--------------------------------------------------------------------------------------------------------------
+@bot.command()
+async def clear(ctx, amountOfMessages = 100):
+    await ctx.channel.purge(limit=amountOfMessages)
+
+
+#-------------------------------------------------------------------------------------------------------------
 bot.run(settings['token']) #токен бота
 # 856526851450339331 id "общее"
